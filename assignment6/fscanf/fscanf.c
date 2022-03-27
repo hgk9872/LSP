dfc1c56f1aaeb3b3fb215b9da88637b2f85c5739
hgk9872 <hgk9872@gmail.com>
2022-03-27 00:52:17 -0700

    assignment6 commit

---
+++ assignment6/fscanf/fscanf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#define BUFFER_SIZE 1024
+
+int main(void)
+{
+    char *fname = "ssu_test.dat";
+    char name[BUFFER_SIZE];
+    FILE *fp;
+    int age;
+
+    fp = fopen(fname, "r");
+    fscanf(fp, "%s%d", name, &age);
+    fclose(fp);
+    fp = fopen(fname, "w");
+    fprintf(fp, "%s is %d years old\n", name, age);
+    fclose(fp);
+    exit(0);
+}
